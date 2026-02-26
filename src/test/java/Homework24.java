348d5995650afc790ed6a3da9b2263e784bfcfbe
ccvn010 <calvin.nguyen@testpro.io>
2026-02-26 11:54:03 -0800

    Update Homework24.java

--- src/test/java/Homework24.java
+++ src/test/java/Homework24.java
@@ -1,27 +1,26 @@
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertTrue;
 
-public class Homework24 {
+public class Homework24Test {
 
     private WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
         // Obtain a Chrome session from the local Grid
-        driver = BrowserFactory.getDriver();
+        driver = BrowserFactory.createChromeDriver();
         driver.manage().window().maximize();
     }
 
     @Test
-    public void testKoelLoginPage() {
+    public void testKoelHomePage() {
         driver.get("https://qa.koel.app/");
-        String title = driver.getTitle();
-        // You can replace with a more meaningful assertion
-        assertTrue(title.contains("Koel") || driver.getPageSource().contains("Log In"),
-                   "Koel page did not load correctly");
+        String pageSource = driver.getPageSource();
+        // Simple assertion: the page should contain "Log In"
+        Assert.assertTrue(pageSource.contains("Log In"), "Koel login page not loaded");
     }
 
     @AfterMethod
