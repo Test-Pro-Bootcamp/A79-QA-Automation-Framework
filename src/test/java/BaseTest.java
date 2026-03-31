7728b416a93e24f59051120666adf9d477d5786f
rumbosc <andrey.klimov@testpro.io>
2026-03-31 13:37:33 -0400

    class changes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,61 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.sql.Driver;
+import java.time.Duration;
+
 public class BaseTest {
+    public WebDriver driver;
+    public String url = "https://qa.koel.app/";
+
 
     @BeforeSuite
     static void setupClass() {
+
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void launchBrowser(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+    public void navigatingToPage() {
+        driver.get(url);
+    }
+
+    public void clickOnLoginButton() {
+        WebElement loginbtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginbtn.click();
+    }
+
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(email);
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,27 +1,28 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
     public void loginEmptyEmailPassword() {
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        // TODO (for students): Review the configuration as part of HW15
         
-        String url = "httpps://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        //Navigating
+        navigatingToPage();
+        navigatingToPage();
+        //Assertion
+        Assert.assertEquals(driver.getCurrentUrl(),url);
     }
+
+
+
+    @Test
+    public void loginValidEmailPassword() throws InterruptedException{
+        navigatingToPage();
+        provideEmail("andrey.klimov@testpro.io");
+        providePassword("W*836553466485uh");
+        clickOnLoginButton();
+        Thread.sleep(2000);
+    }
+
+
 }
